5315e435c94744856707b9ecac6f6d702140c111
liuzhao2580 <1457639270@qq.com>
2020-11-18 20:17:37 +0800

    添加 文档页面 react 和antd 的官方地址链接

--- src/views/document/index.jsx
+++ src/views/document/index.jsx
@@ -1,8 +1,52 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { List, Card } from 'antd'
+const { Meta } = Card
 
 const Document = () => {
+    const [documentList] = useState(() => {
+        return [
+            {
+                url: 'https://react.docschina.org/docs/getting-started.html',
+                img: 'https://react.docschina.org/favicon.ico',
+                title: 'React'
+            },
+            {
+                url: 'https://ant-design.gitee.io/index-cn',
+                img: 'https://gw.alipayobjects.com/zos/rmsportal/KDpgvguMpGfqaHPjicRK.svg',
+                title: 'Ant Design'
+            }
+        ]
+    })
     return (
-        <div>Document</div>
+        <List
+            grid={{
+                gutter: 10,
+                xs: 1,
+                sm: 2,
+                md: 2,
+                lg: 2,
+                xl: 4,
+                xxl: 4
+            }}
+            dataSource={documentList}
+            renderItem={item => (
+                <List.Item>
+                    <Card
+                        style={{ cursor: 'pointer',textAlign: "center" }}
+                        onClick={() => window.open(item.url)}
+                        cover={
+                            <img
+                                style={{width: '50%',height: '50%',display: 'inline-table',marginTop: 20}}
+                                alt='example'
+                                src={item.img}
+                            />
+                        }
+                    >
+                        <Meta title={item.title} description={item.title} />
+                    </Card>
+                </List.Item>
+            )}
+        />
     )
 }
 
