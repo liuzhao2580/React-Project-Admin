5550f57976b564806ed657b7ddb6760f1f487efb
小火车况且况且 <1457639270@qq.com>
2021-06-05 16:23:20 +0800

    首页折线图出现 屏幕监听变化完成

--- src/components/EchartsCom/index.tsx
+++ src/components/EchartsCom/index.tsx
@@ -6,32 +6,35 @@ const EchartsCom = prop => {
   /** 设置 echarts的id */
   let echartId =
     'echartId-' + new Date().getTime() + Math.floor(Math.random() * 10000)
-  // /** 获取 echarts的dom 元素 */
-  // let [echartDom, setEchartDom] = useState<HTMLElement | null>()
-  // /** 获取 echarts的实例化对象 */
-  // let myChart: any = null
+  /** 获取 echarts的dom 元素 */
+  let [echartDom, setEchartDom] = useState<HTMLElement | null>()
+  /** 获取 echarts的实例化对象 */
+  let myChart: any = null
 
-  // /** 监听屏幕的变化 */
-  // // const windowResizeEcharts = useCallback(() => {
-  // //   console.log(myChart, '123')
-  // //   // myChart.resize()
-  // // }, [myChart])
+  /** 初始化 echarts */
+  const initEcharts = () => {
+    setEchartDom(document.getElementById(`${echartId}`))
+    if (!echartDom) return
+    if (myChart) myChart.clear()
+    myChart = Echarts.init(echartDom)
+    myChart.setOption(prop.option)
+  }
+  const getU = useCallback(initEcharts, [echartDom])
+  useEffect(() => {
+    getU()
+  })
 
-  // /** 初始化 echarts */
-  // const initEcharts = () => {
-  //   setEchartDom(document.getElementById(`${echartId}`))
-  //   if (!echartDom) return
-  //   if (myChart) myChart.clear()
-  //   myChart = Echarts.init(echartDom)
-  //   myChart.setOption(prop.option)
-  //   console.log(myChart, 'myChart')
-  //   // window.addEventListener('resize', windowResizeEcharts)
-  // }
-  // const getU = useCallback(initEcharts, [echartDom])
-  // useEffect(() => {
-  //   getU()
-  //   console.log(111111)
-  // })
+  /** 监听屏幕的变化 */
+  const windowResizeEcharts = () => {
+    if (myChart) myChart.resize()
+  }
+  useEffect(() => {
+    window.addEventListener('resize', windowResizeEcharts)
+    return () => {
+      window.removeEventListener('resize', windowResizeEcharts)
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  })
   return (
     <div
       id={echartId}
