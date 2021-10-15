904b77686ef144696739feee964d46d32cd6c344
liuzhao2580 <1457639270@qq.com>
2021-10-15 10:23:55 +0800

    :art: 用户头像编辑

--- src/views/personal/components/HeaderCom/ChangeAvatarCom.tsx
+++ src/views/personal/components/HeaderCom/ChangeAvatarCom.tsx
@@ -1,35 +1,29 @@
 import React, { useState, FC } from 'react'
-import { Modal, Upload } from 'antd'
+import { Upload } from 'antd'
 
 import ImgCrop from 'antd-img-crop'
 
 import './avatar-upload.scss'
 
 interface ICom {
-  modalVisible: boolean
-  setModalVisible: Function
+  avatar: string
 }
 
-const ChangeAvatarCom: FC<ICom> = ({
-  modalVisible,
-  setModalVisible
-}) => {
-  const handleOk = () => {
-    setModalVisible(false)
-  }
-
-  const handleCancel = () => {
-    setModalVisible(false)
-  }
-
-  const [fileList, setFileList] = useState([])
+const ChangeAvatarCom: FC<ICom> = ({ avatar }) => {
+  const [fileList, setFileList] = useState([
+    {
+      uid: 'uid', // 文件唯一标识，建议设置为负数，防止和内部产生的 id 冲突
+      name: 'xx.png', // 文件名removed，被 beforeUpload 拦截的文件没有 status 属性
+      url: avatar
+    }
+  ])
 
   const onChange = ({ fileList: newFileList }) => {
     setFileList(newFileList)
   }
 
   const onPreview = async file => {
-    console.log(file, "file")
+    console.log(file, 'file')
     let src = file.url
     if (!src) {
       src = await new Promise(resolve => {
@@ -45,32 +39,21 @@ const ChangeAvatarCom: FC<ICom> = ({
   }
 
   return (
-    <>
-      <Modal
-        title="更换头像"
-        visible={modalVisible}
-        onOk={handleOk}
-        onCancel={handleCancel}
-        width="400px"
-        className="personal-header-modal-box"
+    <ImgCrop rotate>
+      <Upload
+        action=""
+        listType="picture-card"
+        fileList={fileList}
+        onChange={onChange}
+        onPreview={onPreview}
+        maxCount={1}
+        className="user-avatar-upload"
       >
-        <ImgCrop rotate>
-          <Upload
-            action=""
-            listType="picture-card"
-            fileList={fileList}
-            onChange={onChange}
-            onPreview={onPreview}
-            maxCount={1}
-            className="user-avatar-upload"
-          >
-            {fileList.length === 0 && (
-              <div className="user-avatar-upload-add">+ Upload</div>
-            )}
-          </Upload>
-        </ImgCrop>
-      </Modal>
-    </>
+        {fileList.length === 0 && (
+          <div className="user-avatar-upload-add">+ Upload</div>
+        )}
+      </Upload>
+    </ImgCrop>
   )
 }
 export default ChangeAvatarCom
--- src/views/personal/components/HeaderCom/index.tsx
+++ src/views/personal/components/HeaderCom/index.tsx
@@ -3,29 +3,16 @@ import ChangeAvatarCom from './ChangeAvatarCom'
 
 /** 个人中心页面 */
 const PersonalHeader = ({ userInfo }) => {
-  const [modalVisible, setModalVisible] = useState<boolean>(false)
-  /**
-   * 更换头像
-   */
-  const changeAvatar = () => {
-    setModalVisible(true)
-  }
   return (
     <>
       <header className="personal-header-box">
         <div className="header-img-box">
-          <img src={userInfo.avatar} alt="" className="avatar-box" />
-          <span className="img-hover-bgc" onClick={changeAvatar}>
-            更换头像
-          </span>
+          <ChangeAvatarCom avatar={userInfo.avatar}/>
         </div>
         <p className="nick-name">{userInfo.nickName}</p>
       </header>
 
-      <ChangeAvatarCom
-        modalVisible={modalVisible}
-        setModalVisible={() => setModalVisible(false)}
-      />
+      
     </>
   )
 }
