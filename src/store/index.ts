ab1851dcc4710ca19a640414554937e4cb8ce24a
小火车况且况且 <13277366252@163.com>
2021-10-26 21:11:35 +0800

    :sparkles: 使用webpack中自动导入的方式处理store中的模块

--- src/store/index.ts
+++ src/store/index.ts
@@ -2,16 +2,21 @@ import { combineReducers } from 'redux'
 import { createStore, applyMiddleware, compose } from 'redux'
 // import logger from "redux-logger"
 import thunk from 'redux-thunk'
-import todoList from './modules/todoList/reducers'
-import app from './modules/app/reducers'
-import user from './modules/user/reducers'
 
 
-const rootReducer = combineReducers({
-  todoList,
-  app,
-  user
-})
+const autoRequiredModule = {}
+
+function importAll(r) {
+  r.keys().forEach((key:string) => {
+    const getKey = key.split('/')[1]
+    autoRequiredModule[getKey] = r(key).default
+  })
+}
+// @ts-ignore
+// 可以获取当前指定文件夹下面的文件,第二个参数设置为true,代表递归寻找
+importAll(require.context('./modules', true, /reducers\.ts$/))
+
+const rootReducer = combineReducers(autoRequiredModule)
 const setWindow: any = window
 const composeHandle = setWindow.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
   ? setWindow.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({})
