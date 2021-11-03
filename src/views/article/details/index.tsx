2fb5a358fd6bff66ce8e699e718d709121707444
小火车况且况且 <13277366252@163.com>
2021-11-03 11:44:13 +0800

    :art: 文章详情渲染完成

--- src/views/article/details/index.tsx
+++ src/views/article/details/index.tsx
@@ -16,7 +16,6 @@ const ArticleDetails = props => {
   useEffect(() => {
     ;(async function () {
       const data = await getArticleDetailsByIdApi(urlParams.id)
-      console.log(data)
       if (data.code === ResultCodeEnum.SUCCESS) {
         setArticleDetails(data.data)
       }
@@ -24,53 +23,56 @@ const ArticleDetails = props => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
   return (
-    <div className="article-details-com">
+    <>
       {articleDetails && (
-        <div className="article-details-com-header">
-          <div className="article-details-com-header-title">
-            {articleDetails.title}
-          </div>
-          <div className="article-details-com-header-main">
-            <div className="article-details-com-header-main-left">
-              <div className="article-details-com-header-main-left-top">
-                <div className="article-details-com-header-main-left-top-nickName mr10">
-                  {articleDetails.nickName}
+        <div className="article-details-com">
+          <div className="article-details-com-header">
+            <div className="article-details-com-header-title">
+              {articleDetails.title}
+            </div>
+            <div className="article-details-com-header-main">
+              <div className="article-details-com-header-main-left">
+                <div className="article-details-com-header-main-left-top">
+                  <div className="article-details-com-header-main-left-top-nickName mr10">
+                    {articleDetails.nickName}
+                  </div>
+                  <div className="article-details-com-header-main-left-top-update-time">
+                    {articleDetails.updateTime}
+                  </div>
                 </div>
-                <div className="article-details-com-header-main-left-top-update-time">
-                  {articleDetails.updateTime}
+                <div className="article-details-com-header-main-left-bottom mt10">
+                  <div className="article-details-com-header-main-left-bottom-category mr10">
+                    <span className="article-details-com-header-main-left-bottom-category-title mr10">
+                      一级分类:
+                    </span>
+                    <span className="article-details-com-header-main-left-bottom-category-name">
+                      {articleDetails.categoryParentName}
+                    </span>
+                  </div>
+                  <div className="article-details-com-header-main-left-bottom-category">
+                    <span className="article-details-com-header-main-left-bottom-category-title mr10">
+                      二级分类:
+                    </span>
+                    <span className="article-details-com-header-main-left-bottom-category-name">
+                      {articleDetails.categoryName}
+                    </span>
+                  </div>
                 </div>
               </div>
-              <div className="article-details-com-header-main-left-bottom mt10">
-                <div className="article-details-com-header-main-left-bottom-category mr10">
-                  <span className="article-details-com-header-main-left-bottom-category-title mr10">
-                    一级分类:
-                  </span>
-                  <span className="article-details-com-header-main-left-bottom-category-name">
-                    {articleDetails.categoryParentName}
-                  </span>
+              <div className="article-details-com-header-main-right">
+                <div className="article-details-com-header-main-right-status">
+                  <ArticleStatusCom status={articleDetails.status} />
                 </div>
-                <div className="article-details-com-header-main-left-bottom-category">
-                  <span className="article-details-com-header-main-left-bottom-category-title mr10">
-                    二级分类:
-                  </span>
-                  <span className="article-details-com-header-main-left-bottom-category-name">
-                    {articleDetails.categoryName}
-                  </span>
+                <div className="article-details-com-header-main-right-edit">
+                  编辑
                 </div>
               </div>
             </div>
-            <div className="article-details-com-header-main-right">
-              <div className="article-details-com-header-main-right-status">
-                <ArticleStatusCom status={articleDetails.status} />
-              </div>
-              <div className="article-details-com-header-main-right-edit">
-                编辑
-              </div>
-            </div>
           </div>
+          <div className="article-details-com-container" dangerouslySetInnerHTML={{__html: articleDetails.content}}></div>
         </div>
       )}
-    </div>
+    </>
   )
 }
 
