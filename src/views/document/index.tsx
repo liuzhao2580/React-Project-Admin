c4f2baa9623439eb6fd364b8a956e6067c45d780
小火车况且况且 <13277366252@163.com>
2022-01-26 15:04:35 +0800

    :fire: 修改文档页面为ts文件格式

---
+++ src/views/document/index.tsx
@@ -0,0 +1,63 @@
+import { useState } from 'react'
+import funnyPng from '@/assets/images/funny.png'
+import { List, Card } from 'antd'
+const { Meta } = Card
+
+
+const Document = () => {
+  const [documentList] = useState(() => [
+    {
+      url: 'https://react.docschina.org/docs/getting-started.html',
+      img: 'https://react.docschina.org/favicon.ico',
+      title: 'React'
+    },
+    {
+      url: 'https://ant-design.gitee.io/components/overview-cn/',
+      img: 'https://gw.alipayobjects.com/zos/rmsportal/KDpgvguMpGfqaHPjicRK.svg',
+      title: 'Ant Design'
+    },
+    {
+      url: 'https://blog.csdn.net/weixin_43972992?type=blog',
+      img: funnyPng,
+      title: 'CSND博客地址'
+    }
+  ])
+  return (
+    <List
+      grid={{
+        gutter: 10,
+        xs: 1,
+        sm: 2,
+        md: 2,
+        lg: 2,
+        xl: 4,
+        xxl: 4
+      }}
+      dataSource={documentList}
+      renderItem={item => (
+        <List.Item>
+          <Card
+            style={{ cursor: 'pointer', textAlign: 'center' }}
+            onClick={() => window.open(item.url)}
+            cover={
+              <img
+                style={{
+                  width: '50%',
+                  height: '50%',
+                  display: 'inline-table',
+                  marginTop: 20
+                }}
+                alt="example"
+                src={item.img}
+              />
+            }
+          >
+            <Meta title={item.title} description={item.title} />
+          </Card>
+        </List.Item>
+      )}
+    />
+  )
+}
+
+export default Document
